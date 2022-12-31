5462c2fdf067ea441aadf84f6d4cee295523b08d
aneesh202 <potturianeesh@gmail.com>
2022-12-31 07:47:25 +0530

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TextAreaBoxComponent } from './text-area-box/text-area-box.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent, TextAreaBoxComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/text-area-box/text-area-box.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-area-box',
+  templateUrl: './text-area-box.component.html',
+  styleUrls: ['./text-area-box.component.css']
+})
+export class TextAreaBoxComponent implements OnInit {
+  public textAreaForm: FormGroup;
+
+  //Output Emitters
+	@Output() updateText = new EventEmitter<any>();
+  //Input Bindings
+	@Input() rows: number =10;
+	@Input() inputLabel: string = "";
+	@Input() statusText: string  = "";
+  @Input() text: string = "";
+  @Input() maxCount: number = 50;
+  constructor(private fb : FormBuilder){
+  }
+
+  ngOnInit() {
+    this.textAreaForm = this.fb.group({
+      textArea: this.text
+    });
+  }
+  update =(data)=>{
+    this.updateText.emit(data);
+  }
+}
